373d2b78f90c891aec4b2bf49d516b665852134c
Reeva Gupta <114238405+reevacodes@users.noreply.github.com>
2025-08-01 23:56:54 +0530

    Add files via upload

---
+++ leetcode328.cpp
@@ -0,0 +1,61 @@
+// ODD EVEN LL
+
+#include <iostream>
+using namespace std;
+
+// Definition for singly-linked list
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+// Function to rearrange list: odd indices first, then even
+ListNode* oddEvenList(ListNode* head) {
+    if (!head || !head->next) return head;
+
+    ListNode* odd = head;
+    ListNode* even = head->next;
+    ListNode* evenHead = even;
+
+    while (even && even->next) {
+        odd->next = even->next;
+        odd = odd->next;
+
+        even->next = odd->next;
+        even = even->next;
+    }
+
+    odd->next = evenHead;
+    return head;
+}
+
+// Helper function to print the linked list
+void printList(ListNode* head) {
+    while (head) {
+        cout << head->val;
+        if (head->next) cout << " -> ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+// Main function for testing
+int main() {
+    // Creating the list: 1 -> 2 -> 3 -> 4 -> 5
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    cout << "Original list:\n";
+    printList(head);
+
+    head = oddEvenList(head);
+
+    cout << "After rearranging odd-even:\n";
+    printList(head);
+
+    return 0;
+}
